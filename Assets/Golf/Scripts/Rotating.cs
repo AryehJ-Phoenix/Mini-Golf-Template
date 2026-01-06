b0cf5c585df02d98e0f7f3883ccd2d764280e849
Aryeh J - Phoenix <ajacobowitz1323@STUDENT.PPS.NET>
2026-01-06 13:55:56 -0800

    first try!!

---
+++ Assets/Golf/Scripts/Rotating.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+
+{
+    public float xAngle,yAngle,zAngle;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotating.transform.Rotate(xAngle,yAngle,zAngle,Space.Self)
+    }
+}
